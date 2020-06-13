edfeeab4f825f9d063ff292a51f5e2bbab852155
vinaykudari <vinay.kudari30@gmail.com>
2020-06-14 02:10:09 +0530

    graphs week5 q1

--- graphs/week5_mst/1_connecting_points/connecting_points.py
+++ graphs/week5_mst/1_connecting_points/connecting_points.py
@@ -1,11 +1,42 @@
 #Uses python3
 import sys
 import math
+import heapq
+
 
 def minimum_distance(x, y):
-    result = 0.
-    #write your code here
-    return result
+    N = len(x)
+    heap = []
+
+    cost = [float('inf')] * N
+    parent = [None] * N
+
+    cost[0] = 0
+    total_cost = 0
+    nodes_to_check = list(range(N))
+
+    for i in range(N):
+        heapq.heappush(heap, (cost[i], i))
+
+    while heap:
+        uCost, uIndex = heapq.heappop(heap)
+        if uIndex in nodes_to_check:
+            nodes_to_check.remove(uIndex)
+        if uCost > cost[uIndex]:
+            continue
+        total_cost += uCost
+        for vIndex in nodes_to_check:
+            vCost = get_dist(x[uIndex], y[uIndex], x[vIndex], y[vIndex])
+            if cost[vIndex] > vCost:
+                cost[vIndex] = vCost
+                parent[vIndex] = uIndex
+                heapq.heappush(heap, (vCost, vIndex))
+
+    return total_cost
+
+
+def get_dist(x1, y1, x2, y2):
+    return math.sqrt(math.pow((y2-y1), 2) + math.pow((x2-x1), 2))
 
 
 if __name__ == '__main__':
