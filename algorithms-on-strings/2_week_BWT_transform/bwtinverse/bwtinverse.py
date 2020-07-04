fe6eec88640e2a328746c1913be76ff4ba9e6ea2
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-04 21:16:29 +0530

    bwt inverse with hash map

--- algorithms-on-strings/2_week_BWT_transform/bwtinverse/bwtinverse.py
+++ algorithms-on-strings/2_week_BWT_transform/bwtinverse/bwtinverse.py
@@ -1,9 +1,34 @@
 # python3
 import sys
 
+
+def get_text_with_index(text):
+    hash_map = {}
+    text_with_index = []
+    for ch in text:
+        hash_map[ch] = hash_map.get(ch, 0) + 1
+        text_with_index.append(ch + str(hash_map[ch]))
+
+    return text_with_index
+
+
 def InverseBWT(bwt):
     # write your code here
-    return ""
+    hash_map = {}
+    result = ''
+    bwt_with_index = get_text_with_index(bwt)
+    first_col_with_index = get_text_with_index(sorted(bwt))
+
+    for i in range(len(bwt)):
+        hash_map[first_col_with_index[i]] = bwt_with_index[i]
+
+    ch = '$1'
+
+    while hash_map[ch] != '$1':
+        result += hash_map[ch][0]
+        ch = hash_map[ch]
+
+    return result[::-1] + '$'
 
 
 if __name__ == '__main__':
