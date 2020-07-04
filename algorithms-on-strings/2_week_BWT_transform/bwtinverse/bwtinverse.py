325f3799764114bddc9298077c045f6587bfbec5
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-05 01:29:51 +0530

    optimise bwt inverse with counting sort

--- algorithms-on-strings/2_week_BWT_transform/bwtinverse/bwtinverse.py
+++ algorithms-on-strings/2_week_BWT_transform/bwtinverse/bwtinverse.py
@@ -12,23 +12,55 @@ def get_text_with_index(text):
     return text_with_index
 
 
+def count_sort(arr, arr_len):
+    min_index, max_index = min(arr), max(arr)
+    res = [0] * arr_len
+
+    for i in range(arr_len):
+        arr[i] -= min_index
+
+    count_arr = [0]*(max_index-min_index+1)
+    for i in arr:
+        count_arr[i] += 1
+
+    cum_sum = 0
+    for i in range(len(count_arr)):
+        cum_sum += count_arr[i]
+        count_arr[i] = cum_sum
+
+    for i in count_arr[::-1]:
+        res[i-1] = chr(max_index)
+        max_index -= 1
+
+    for i in range(arr_len-2, -1, -1):
+        if res[i] == 0:
+            res[i] = res[i+1]
+
+    return ''.join(res)
+
+
+def get_int_array_from_text(text):
+    return[ord(ch) for ch in text]
+
+
 def InverseBWT(bwt):
     # write your code here
     hash_map = {}
-    result = ''
-    bwt_with_index = get_text_with_index(bwt)
-    first_col_with_index = get_text_with_index(sorted(bwt))
+    text_len = len(bwt)
+    result = []
 
-    for i in range(len(bwt)):
+    bwt_with_index = get_text_with_index(bwt)
+    first_col_with_index = get_text_with_index(count_sort(get_int_array_from_text(bwt), text_len))
+    for i in range(text_len):
         hash_map[first_col_with_index[i]] = bwt_with_index[i]
 
     ch = '$1'
 
     while hash_map[ch] != '$1':
-        result += hash_map[ch][0]
+        result.append(hash_map[ch][0])
         ch = hash_map[ch]
 
-    return result[::-1] + '$'
+    return ''.join(result[::-1]) + '$'
 
 
 if __name__ == '__main__':
