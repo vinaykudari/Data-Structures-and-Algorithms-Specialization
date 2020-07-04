6db503ff5c85d27b8b6bdcc06e52e2295b45ed41
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2020-07-04 20:22:47 +0530

    bwt transform

--- algorithms-on-strings/2_week_BWT_transform/bwt/bwt.py
+++ algorithms-on-strings/2_week_BWT_transform/bwt/bwt.py
@@ -3,7 +3,16 @@ import sys
 
 
 def BWT(text):
-    pass
+    bwt_matrix = []
+    i = 0
+
+    while i < len(text):
+        text = text[-1] + text[:-1]
+        bwt_matrix.append(text)
+        i += 1
+    bwt_matrix.sort()
+
+    return ''.join([text[-1] for text in bwt_matrix])
 
 
 if __name__ == '__main__':
